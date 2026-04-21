8e8fcf89aa0c499e1678339a0d43c7f6c3d54caf
realman-li <92239371+realman-li@users.noreply.github.com>
2026-04-21 21:16:30 +0800

    Update MyApplication.java

--- app/src/main/java/com/example/scanmaster/MyApplication.java
+++ app/src/main/java/com/example/scanmaster/MyApplication.java
@@ -1,21 +1,11 @@
 package com.example.scanmaster;
 
 import android.app.Application;
-import android.util.Log;
-
-import org.opencv.android.OpenCVLoader;
 
 public class MyApplication extends Application {
-    private static final String TAG = "ScanMaster";
-
     @Override
     public void onCreate() {
         super.onCreate();
-        // 初始化OpenCV远程依赖库
-        if (OpenCVLoader.initLocal()) {
-            Log.d(TAG, "OpenCV 初始化成功");
-        } else {
-            Log.e(TAG, "OpenCV 初始化失败");
-        }
+        // 注释掉所有OpenCV代码
     }
 }
